04216bc4f71ae7d7ec951b1cb0a53004d8aa509b
bhanuprasad <bhanuprasadg54@gmail.com>
2022-01-18 22:03:40 +0530

    Formatted lines

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -3,7 +3,7 @@ public class LinearSearch {
         int[] arr = new int[]{1, 2, 5, 4, 9, 2, 33, 5, 98, 45};
         int element = 33;
         int index = linearSearch(arr, arr.length, element);
-        System.out.println("Element " + element + " found at index "+ index);
+        System.out.println("Element " + element + " found at index " + index);
     }
 
     private static int linearSearch(final int[] arr, final int size, final int element) {
