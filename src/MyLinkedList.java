67d2e93e6f0be0d2364183266e1d043ebed3274a
bhanuprasad <bhanuprasadg54@gmail.com>
2022-03-11 22:15:25 +0530

    Added Linked List Methods 1. Insert node at beginning 2. Insert node after node 3. Insert node at the end

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,35 +1,65 @@
 public class MyLinkedList {
+
+    Node head;
+
     static class Node {
         int data;
         Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
     }
 
-    public static void main(String[] args) {
-        // Create nodes
-        Node head = new Node();
-        Node n2 = new Node();
-        Node n3 = new Node();
-        Node n4 = new Node();
 
-        head.data = 2;
-        head.next = n2;
-
-        n2.data = 45;
-        n2.next = n3;
-
-        n3.data = 67;
-        n3.next = n4;
-
-        n4.data = 19;
-
-        traverseLinkedList(head);
+    public void push(int data) {
+        Node node = new Node(data);
+        if (head != null) {
+            node.next = this.head;
+        }
+        this.head = node;
     }
+
+    public void insertAfter(final Node preNode, final int data) {
+        final Node newNode = new Node(data);
+        newNode.next = preNode.next;
+        preNode.next = newNode;
+    }
+
+    public void insertAtEnd(final int data) {
+        final Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
 
     public static void traverseLinkedList(Node node) {
         while (node != null) {
             System.out.print(node.data + " ");
             node = node.next;
         }
+    }
+
+    public static void main(String[] args) {
+
+        MyLinkedList linkedList = new MyLinkedList();
+        linkedList.push(10);
+        linkedList.push(20);
+        linkedList.push(30);
+        linkedList.push(40);
+
+        linkedList.insertAfter(linkedList.head.next.next, 33);
+
+        linkedList.insertAtEnd(99);
+
+        traverseLinkedList(linkedList.head);
     }
 
 }
