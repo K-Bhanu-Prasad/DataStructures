e21834d4cfb1420de73d3fc028a5f2788a67878d
bhanuprasad <bhanuprasadg54@gmail.com>
2022-03-15 22:23:59 +0530

    Added Linked List Methods 1. delete node at beginning 2. delete node at index 3. delete node with value 4. delete node at the end

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -39,11 +39,49 @@ public class MyLinkedList {
         temp.next = newNode;
     }
 
+    public void deleteFirst() {
+        if (this.head != null) {
+            head = head.next;
+        }
+    }
 
-    public static void traverseLinkedList(Node node) {
-        while (node != null) {
-            System.out.print(node.data + " ");
-            node = node.next;
+    public void deleteAtIndex(final int index) {
+        if (index == 0) {
+            deleteFirst();
+        }
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+    }
+
+    public void deleteWithValue(final int value) {
+        Node temp = head, prev = head;
+        while (temp != null && temp.data != value) {
+            prev = temp;
+            temp = temp.next;
+        }
+        if (temp == null) {
+            System.out.println("\nNo element found with value " + value);
+        } else {
+            prev.next = temp.next;
+        }
+    }
+
+    public void deleteLast() {
+        Node prev = head;
+        while (prev.next.next != null) {
+            prev = prev.next;
+        }
+        prev.next = null;
+    }
+
+    public static void traverseLinkedList(final Node node) {
+        Node temp = node;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
         }
     }
 
@@ -59,6 +97,27 @@ public class MyLinkedList {
 
         linkedList.insertAtEnd(99);
 
+        System.out.println("\nList is :");
+        traverseLinkedList(linkedList.head);
+
+        linkedList.deleteFirst();
+
+        System.out.println("\nAfter deleting first element");
+        traverseLinkedList(linkedList.head);
+
+        linkedList.deleteLast();
+
+        System.out.println("\nAfter deleting last element");
+        traverseLinkedList(linkedList.head);
+
+        linkedList.deleteWithValue(33);
+
+        System.out.println("\nAfter deleting element 33");
+        traverseLinkedList(linkedList.head);
+
+        linkedList.deleteAtIndex(2);
+
+        System.out.println("\nAfter deleting element at index 2");
         traverseLinkedList(linkedList.head);
     }
 
