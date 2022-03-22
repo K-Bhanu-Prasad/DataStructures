dc2e4ea02cb41c8bc0de17e49feb4fc2de86f30a
bhanuprasad <bhanuprasadg54@gmail.com>
2022-03-22 22:09:29 +0530

    Added Circular Linked list methods 1. Insert at end 2. Insert after element

--- src/MyCircularLinkedList.java
+++ src/MyCircularLinkedList.java
@@ -30,6 +30,25 @@ public class MyCircularLinkedList {
         head = node;
     }
 
+    public void insetAtEnd(final int data) {
+        final Node node = new Node(data, head);
+        Node temp = head;
+        while (temp.next != head) {
+            temp = temp.next;
+        }
+        temp.next = node;
+    }
+
+    public void insertAfterElement(final int element, final int data) {
+        final Node node = new Node(data, null);
+        Node temp = this.head;
+        while (temp.data != element) {
+            temp = temp.next;
+        }
+        node.next = temp.next;
+        temp.next = node;
+    }
+
     /***
      * Traverse method print from the head, similar to a stack
      * @param head head
@@ -37,7 +56,7 @@ public class MyCircularLinkedList {
     public static void traverse(final Node head) {
         Node temp = head;
         do {
-            System.out.println(temp.data);
+            System.out.print(temp.data + " ");
             temp = temp.next;
         } while (temp != head);
     }
@@ -48,6 +67,15 @@ public class MyCircularLinkedList {
         list.push(2);
         list.push(4);
 
+        System.out.println("\n List is");
+        traverse(list.head);
+
+        list.insetAtEnd(1);
+        System.out.println("\n After inserting 1 at end:");
+        traverse(list.head);
+
+        list.insertAfterElement(2, 99);
+        System.out.println("\n Inserting 99 after element 2");
         traverse(list.head);
 
     }
